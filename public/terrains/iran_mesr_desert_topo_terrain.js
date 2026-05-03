2b912bb6d6b5f8f87a20e92c2ba8c3a819c535b7
enzoluznunez-create <enzonunez@vt.edu>
2026-05-02 21:29:59 -0700

    Restore Iran terrain JS compatibility module

---
+++ public/terrains/iran_mesr_desert_topo_terrain.js
@@ -0,0 +1,12 @@
+export {
+  SAMALAYUCA_DUNE_FIELDS_TOPO_CONFIG,
+  createSamalayucaDuneFieldsScene,
+  metersToLatLon,
+  renderHeightMetersAt,
+  terrainElevationMetersAt,
+} from "./samalayuca_dune_fields_topo_terrain.js";
+
+export {
+  SAMALAYUCA_DUNE_FIELDS_TOPO_CONFIG as IRAN_MESR_DESERT_TOPO_CONFIG,
+  createSamalayucaDuneFieldsScene as createIranMesrDesertTopoScene,
+} from "./samalayuca_dune_fields_topo_terrain.js";
