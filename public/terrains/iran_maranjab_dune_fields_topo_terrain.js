0d5f0303c36c42bb63509aed915571aafb084014
enzoluznunez-create <enzonunez@vt.edu>
2026-05-03 00:11:38 -0700

    Set terrain sky fog to light blue

--- public/terrains/iran_maranjab_dune_fields_topo_terrain.js
+++ public/terrains/iran_maranjab_dune_fields_topo_terrain.js
@@ -22,7 +22,8 @@ const HALF_TERRAIN = TERRAIN_SIZE / 2;
 const GRID_SEGMENTS = 220;
 const VERTICAL_EXAGGERATION = CONFIG.verticalExaggeration;
 const SEED = 34300518;
-const FOG_COLOR = 0xdcd2bd;
+const SKY_COLOR = 0xd7eefb;
+const FOG_COLOR = SKY_COLOR;
 const RADIAL_FOG = Object.freeze({
   innerRadiusMeters: 500,
   outerRadiusMeters: 1000,
@@ -85,7 +86,7 @@ export function createIranMaranjabDuneFieldsScene(container = document.body, opt
 
   const scene = new THREE.Scene();
   scene.name = CONFIG.name;
-  scene.background = new THREE.Color(FOG_COLOR);
+  scene.background = new THREE.Color(SKY_COLOR);
   scene.fog = new THREE.Fog(FOG_COLOR, 540, 1180);
 
   const camera = new THREE.PerspectiveCamera(54, 1, CAMERA_COLLISION.nearPlaneMeters, 2400);
--- public/terrains/thompson_pass_snow_topo_terrain.js
+++ public/terrains/thompson_pass_snow_topo_terrain.js
@@ -22,7 +22,8 @@ const HALF_TERRAIN = TERRAIN_SIZE / 2;
 const GRID_SEGMENTS = 220;
 const VERTICAL_EXAGGERATION = CONFIG.verticalExaggeration;
 const SEED = 61131302;
-const FOG_COLOR = 0xdce4e7;
+const SKY_COLOR = 0xd7eefb;
+const FOG_COLOR = SKY_COLOR;
 const RADIAL_FOG = Object.freeze({
   innerRadiusMeters: 500,
   outerRadiusMeters: 1000,
@@ -94,7 +95,7 @@ export function createThompsonPassSnowTopoScene(container = document.body, optio
 
   const scene = new THREE.Scene();
   scene.name = CONFIG.name;
-  scene.background = new THREE.Color(FOG_COLOR);
+  scene.background = new THREE.Color(SKY_COLOR);
   scene.fog = new THREE.Fog(FOG_COLOR, 540, 1180);
 
   const camera = new THREE.PerspectiveCamera(54, 1, CAMERA_COLLISION.nearPlaneMeters, 2400);
